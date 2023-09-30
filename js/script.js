c19c00f0cb8950f8a72305bba40e26649c5b2c5d
unknown <brunocesar.tenorio1@gmail.com>
2023-09-30 17:41:41 -0300

    fixing css

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
-export default function initCountdown() {
-  const days = document.querySelector("[data-days]");
-  const hours = document.querySelector("[data-hours]");
-  const minutes = document.querySelector("[data-minutes]");
-  const seconds = document.querySelector("[data-seconds]");
-  if (days && hours && minutes && seconds) {
-    function countdown() {
-      const newYearDate = new Date(2024, 0, 1);
-      const currentDate = new Date();
-      const totalSeconds = (newYearDate - currentDate) / 1000;
+const days = document.querySelector("[data-days]");
+const hours = document.querySelector("[data-hours]");
+const minutes = document.querySelector("[data-minutes]");
+const seconds = document.querySelector("[data-seconds]");
+if (days && hours && minutes && seconds) {
+  function countdown() {
+    const newYearDate = new Date(2024, 0, 1);
+    const currentDate = new Date();
+    const totalSeconds = (newYearDate - currentDate) / 1000;
 
-      const daysRemaining = Math.floor(totalSeconds / 3600 / 24);
-      const hoursRemaining = Math.floor(totalSeconds / 3600) % 24;
-      const minutesRemaining = Math.floor(totalSeconds / 60) % 60;
-      const secondsRemaining = Math.floor(totalSeconds) % 60;
+    const daysRemaining = Math.floor(totalSeconds / 3600 / 24);
+    const hoursRemaining = Math.floor(totalSeconds / 3600) % 24;
+    const minutesRemaining = Math.floor(totalSeconds / 60) % 60;
+    const secondsRemaining = Math.floor(totalSeconds) % 60;
 
-      days.textContent = formatDate(daysRemaining);
-      hours.textContent = formatDate(hoursRemaining);
-      minutes.textContent = formatDate(minutesRemaining);
-      seconds.textContent = formatDate(secondsRemaining);
-    }
-    function formatDate(time) {
-      return time < 10 ? `0${time}` : `${time}`;
-    }
-    setInterval(countdown, 1000);
+    days.textContent = formatDate(daysRemaining);
+    hours.textContent = formatDate(hoursRemaining);
+    minutes.textContent = formatDate(minutesRemaining);
+    seconds.textContent = formatDate(secondsRemaining);
   }
+  function formatDate(time) {
+    return time < 10 ? `0${time}` : `${time}`;
+  }
+  setInterval(countdown, 1000);
 }
